be098d32a59221b25639bf2e2ff06373b842fe36
Logan Snopek <LSnopek@lcs.on.ca>
2021-11-24 11:31:53 -0500

    Initial Commit

---
+++ TranclaterApp/TranclaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranclaterAppApp.swift
+//  TranclaterApp
+//
+//  Created by Logan Snopek on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct TranclaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
